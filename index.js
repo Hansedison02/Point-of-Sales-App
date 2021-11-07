b55d5145bba36947b561cbd757bdd3645bc05bdc
Hansedison02 <hans.535200041@stu.untar.ac.id>
2021-11-07 15:11:52 +0700

    Moved a damn thing again...

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 
-
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
 
 app.listen('3000', ()=> {
     console.log('Server is running on port 3000')
 })
 
-app.get('/', function (req, res) {
-    res.render('pages/index');
-});
 
